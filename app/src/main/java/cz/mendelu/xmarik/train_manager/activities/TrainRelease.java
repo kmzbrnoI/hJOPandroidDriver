830aebe592d35f71ac7b10a6de4ac9ad746a3531
Jan Horacek <jan.horacek@seznam.cz>
2017-05-11 21:05:49 +0200

    TrainRelease uses NavigationBase.

--- app/src/main/java/cz/mendelu/xmarik/train_manager/activities/TrainRelease.java
+++ app/src/main/java/cz/mendelu/xmarik/train_manager/activities/TrainRelease.java
@@ -5,15 +5,11 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -37,8 +33,7 @@ import cz.mendelu.xmarik.train_manager.events.FreeEvent;
 import cz.mendelu.xmarik.train_manager.events.RefuseEvent;
 import cz.mendelu.xmarik.train_manager.events.TrainReloadEvent;
 
-public class TrainRelease extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class TrainRelease extends NavigationBase {
 
     Server active;
     int port;
@@ -57,19 +52,14 @@ public class TrainRelease extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_train_release);
+        super.onCreate(savedInstanceState);
 
-        connectionDialog = new AlertDialog.Builder(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+
+        connectionDialog = new AlertDialog.Builder(this);
+
         lokos = new LinkedList<>();
         context = this;
         trains = (ListView) findViewById(R.id.acquiredTrains);
@@ -189,50 +179,6 @@ public class TrainRelease extends AppCompatActivity
             EventBus.getDefault().unregister(this);
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_server) {
-            Intent intent = new Intent(this, ServerSelect.class);
-            startActivity(intent);
-        } else if (id == R.id.nav_about) {
-            Intent intent = new Intent(this, About.class);
-            startActivity(intent);
-
-        } else if (id == R.id.nav_train_manage) {
-            Intent intent = new Intent(this, TrainHandler.class);
-            startActivity(intent);
-
-        } else if (id == R.id.nav_ack_trains) {
-            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-            drawer.closeDrawer(GravityCompat.START);
-
-        } else if (id == R.id.nav_view) {
-            Intent intent = new Intent(this, ServerSelect.class);
-            startActivity(intent);
-
-        } else if (id == R.id.nav_trains) {
-            Intent intent = new Intent(this, TrainRequest.class);
-            startActivity(intent);
-
-        }
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 
     @Override
